369c28f33f9f1d07ef4933308a45a353702f4a5d
Sah19-coder <alievich@gmail.com>
2021-12-21 23:47:33 +0300

    Finish Lesson2

---
+++ Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace HomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите максимальную температуру:");
+            int MaxTemp = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите минимальную температуру:");
+            int MinTemp = int.Parse(Console.ReadLine());
+            int Calc = (MaxTemp + MinTemp) / 2;
+            Console.WriteLine($"Средняя температура= {Calc}");
+        }
+    }
+}
---
+++ Task4/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace HomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int UNP = 300359960;
+            int Reg = 310007752;
+            int KSA = 000453;
+            int OP  = 002378598;
+
+            double Cheese      = 1.34;
+            double Milk        = 1.74;
+            double sourCream   = 2.26;
+
+            string Name = "ОАО МойТорг";
+
+
+            Console.WriteLine($"       {Name}     ");
+            Console.WriteLine("************************************");
+            Console.WriteLine($"УНП                            {UNP}");
+            Console.WriteLine($"РЕГ #                          {Reg}");
+            Console.WriteLine($"# КСА                          {KSA}");
+            Console.WriteLine("         Платежный документ             ");
+            Console.WriteLine("             Чек продажи                ");
+            Console.WriteLine($"Творог классический  5% 180г      2Х{Cheese}");
+            Console.WriteLine($"                                    {2*Cheese}");
+
+            Console.WriteLine($"Сметана Сафийка стакан 15% 400г   1Х{sourCream}");
+            Console.WriteLine($"                                    {sourCream}");
+            Console.WriteLine($"Молоко Сафийка 2.5% 950 мл бутылка 1Х{Milk}");
+            Console.WriteLine($"                                     {Milk}");
+            Console.WriteLine($"ИТОГ                                 {2*Cheese + sourCream + Milk}");
+            Console.WriteLine("ПЛАСТ. КАРТ");
+            Console.WriteLine($"Оператор 01                    #{OP}");
+        }
+    }
+}
