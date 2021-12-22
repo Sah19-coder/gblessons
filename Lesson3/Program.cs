aaae63f901c2e460d4f5430ac565aaf090e9befc
Serg <alievich@gmail.com>
2021-12-22 11:04:25 +0300

    Init

---
+++ Lesson3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * 1. Написать программу, выводящую элементы двумерного массива по диагонали.
+2. Написать программу «Телефонный справочник»: создать двумерный массив 5х2, хранящий список телефонных контактов: первый элемент хранит имя контакта, второй — номер телефона/email.
+3. Написать программу, выводящую введённую пользователем строку в обратном порядке (olleH вместо Hello).
+*«Морской бой»: вывести на экран массив 10х10, состоящий из символов X и O, где Х — элементы кораблей, а О — свободные клетки.
+ * */
+namespace Lesson3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] MyNums   = { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 }, {13,14,15,16 } };
+            int rows        = MyNums.GetUpperBound(0) + 1;
+            int columns     = MyNums.Length / rows; 
+
+                for(int it=0; it < rows; it++)
+                {
+                Console.WriteLine("");
+                for (int it2 =0; it2 <  columns; it2++)
+                    {
+                    if(it2==it) //Мы на диагонали?
+                        Console.Write($"    {MyNums[it, it2]}");
+                    else
+                        Console.Write("    ");
+                    }
+
+                }
+        }
+    }
+}
---
+++ Lesson3Task2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ * 1. Написать программу, выводящую элементы двумерного массива по диагонали.
+2. Написать программу «Телефонный справочник»: создать двумерный массив 5х2, хранящий список телефонных контактов: первый элемент хранит имя контакта, второй — номер телефона/email.
+3. Написать программу, выводящую введённую пользователем строку в обратном порядке (olleH вместо Hello).
+*«Морской бой»: вывести на экран массив 10х10, состоящий из символов X и O, где Х — элементы кораблей, а О — свободные клетки.
+ * */
+namespace Lesson3Task2
+{
+    class Program
+    {
+      public  string[,] TelDic = { {"Alla", "+375295673546"}, { "Bella", "+375332954967" }, { "David", "+375253458956" }, { "Pamella", "+375334891425" },
+                { "Evgen", "+491234758" }, { "Kontora", "alsng@gmail.com" }, { "sister", "+375295894034" }, { "mother", "+375331092639" },
+                { "work", "+3752910983940" }, { "Abraham", "+4920947890" } };
+        public void PrintDic()
+        {
+            
+        }
+        public static bool SeachAndPrintTel(string Name)
+        {
+            return true;
+        }
+        static void Main(string[] args)
+        {
+        Console.WriteLine("Тел. справочник. Введите:\n s для поиска по имени,\n p для печати всего справочника,\n esc для выхода.");
+        ConsoleKeyInfo command = Console.ReadKey();
+            while(command.Key != ConsoleKey.Escape)
+            {
+                switch (command.Key)
+                {
+                    case ConsoleKey.S:
+                        Console.WriteLine("Введите имя абонента для поиска:");
+                        string Name = Console.ReadLine();
+                        if(SeachAndPrintTel(Name);
+                            break;
+                    case ConsoleKey.P:
+                        PrintDic();
+                        break;
+                }
+            }
+            
+        }
+    }
+}
